8608f0cb6195877d89313c42c95a2c7b6d80ce8f
Tak1wa <abu.soft.taka@gmail.com>
2016-12-05 01:36:54 +0900

    add strategy

---
+++ CSharpDesignPattern/10_strategy/StrategyA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDesignPattern._10_strategy
+{
+    class StrategyA : IStrategy
+    {
+        public void StrategyMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
